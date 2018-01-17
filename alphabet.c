15baf11535684fab72cffb86aa7232f49cda5d7a
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-01-17 17:00:20 +0530

    alphabet or not
    check the alphabet

---
+++ alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,a,b;
+printf("\n enter the value\n");
+scanf("%d",&n&a&b);
+a=('a'to'z');
+b=('A'TO 'Z');
+if(a||b)
+{
+printf("\n the value is alphabet);
+else
+printf("\n the value not alphabet);
+}
+getch();
+}
