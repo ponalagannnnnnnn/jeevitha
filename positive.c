6ecd4b71048758e2889072e4104fcf64c9a9277e
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-01-17 16:46:12 +0530

    positive or negative
    to check positive or negative

---
+++ positive.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("\n enter the number\n");
+scanf("%d",&a);
+if(a=0)
+{
+printf("\n the given number is zero\n");
+else
+{
+if(a>0)
+printf("\n the given number is positive\n");
+else
+printf("\n the given number is negative\n");
+}
+}
+getch();
+}
