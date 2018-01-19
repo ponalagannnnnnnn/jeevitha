9194ccfb278a02e162e054ce2e018d5fa4a69543
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-01-19 13:40:07 +0530

    even or odd
    to check even or odd

---
+++ jeevi.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("\n enter the number\n");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("\n the given number is even\n");
+else
+printf(\n" the given number is odd\n");
+}
+getch();
+}
