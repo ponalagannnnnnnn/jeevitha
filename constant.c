621fb7edf0fdbfe38f5b5ef4ea4c9161d727ab5b
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-01-19 10:49:16 +0530

    constant or vowel
    To check the alphabet

---
+++ constant.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char c,lower case,upper case;
+printf("\n Enter the alphabet\n");
+lower case(c=='a'||'e'||'i'||'o||'u);
+upper case(c=='A'||'E'||'I'||'O'||U);
+if(lower case||upper case);
+{
+printf("\nThe given alphabet is vowel\n");
+else
+printf("\n The given alphabet is constant\n");
+}
+getch();
+}
