27123bad3806bbe18ca904751b3f25d085f259a7
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-01-24 16:10:11 +0530

    number
    to check largest number

---
+++ largest number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("\n ente the values\n");
+scanf("%d%d%d,&a,&b,&c);
+if(a>b)
+{
+printf("\n a is largest");
+}
+else if(b>c)
+{
+printf("\n b is largest\n");
+}
+else if(c>b)
+[
+printf("\n c is largest\n");
+}
+getch();
+}
