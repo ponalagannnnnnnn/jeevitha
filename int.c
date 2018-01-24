91bdd7a5b5bea7e4ba54bd830e39ba1833379221
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-01-24 16:59:17 +0530

    k integer
    arrays

---
+++ int.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n[7]={7,6,5,4,3,2,1},i,sum=0,k;
+printf("\n enter the value of k\n");
+scanf("%d",&k);
+for (i=0;i<k;i++);
+{
+sum=sum=n[i];
+}
+printf("\n the number is %d\n",sum);
+getch();
+}
