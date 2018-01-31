c11aa9c2e80e5ea81f7d53b622fc23fa92b6e736
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-01-31 15:49:59 +0530

    n times
    program

---
+++ jeevias.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i,n,sum;
+printf("\n enter the positive integer\n");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum+=i;
+}
+printf("\n sum is %d",sum);
+getch();
+}
