c525489406230ee22ff115b3bff5db781a2e4c5d
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-01-31 15:59:30 +0530

    ktimes
    program

---
+++ jeeviris.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i,k,sum=0;
+printf("\n enter the k values\n");
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+sum=sum+i;
+}
+printf("\n sum is %d",sum);
+getch();
+}
