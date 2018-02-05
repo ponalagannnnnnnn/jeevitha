7b710a34fe28c6a58a2ef5c75c1e055d27faaa25
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-05 16:35:21 +0530

    Create arr.c

---
+++ arr.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int array[100],n,i,largest;
+printf("\n Enter the numbers");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&array[i]);
+}
+mini=array[0];
+for(i=0;i<n;i++)
+{
+if(mini<array[i])
+mini=array[i]);
+}
+printf("\n the largest number in the array=%d",mini);
