b8dce1a3e0056ed8d7edfa1453c4177f4efb6c9d
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-07 19:14:10 +0530

    Rename lar.c to largg.c

---
+++ largg.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int arr[100],i,n,size,temp,largest;
+printf("\n enter the size of the array:");
+scanf("%d",&size);
+for(i=0;i<size;i++)
+{
+scanf("%d",&a[i]);
+}
+largest=a[0];
+for(i=0;i<size;i++)
+{
+if(largest<a[i])
+{
+largest=a[i];
+}
+}
+printf("\n%d",largest);
+getch();
+}
+
