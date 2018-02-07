52bbb1341168d6a898c7bf04c806215db402bbbb
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-07 18:58:35 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int i,n,temp,arr[100];
+printf("\n enter the size");
+scanf("%d",&size);
+printf("'\n enter the array no");
+for(i=0;i<n;i++)
+{
+scanf("%d",&array);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>aj])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("\n the median element is %d",&a[n/2]);
+getch();
+}
