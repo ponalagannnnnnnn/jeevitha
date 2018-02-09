83ffb5833530e35af6cad50b235c3f519f923e6d
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-09 15:43:13 +0530

    Create time.c

---
+++ time.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int hrs,min,n;
+printf("\n Enter the value of n");
+scanf("%d",&n);
+if(n%n==0);
+{
+hrs=n/60;
+min=n%60;
+printf("%d%d",hrs,min);
+}
+getch();
+}
