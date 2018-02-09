a7a0f5de68db1a884edac879fc28dc7acd3837d5
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-09 16:04:28 +0530

    Create stro.c

---
+++ stro.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void amin()
+{
+char str[20],n;
+printf("\n enter the string");
+scanf("%s",str);
+while(str!='0')
+{
+strlen(str);
+}
+printf("\n the no of letters in the string is:%s",strlen(str));
+getch();
+}
