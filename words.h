0c25ecfa67c64cf832b03c2d9c2960d0ce90fdcb
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-09 16:13:20 +0530

    Create words.h

---
+++ words.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[50];
+int i,count=n,c;
+printf("\n enter the string");
+gets(a);
+c=strlen(a);
+if(a[i]==' ')
+{
+count=count+1;
+}
+printf("\n The total words is %d",count);
+getch();
+}
