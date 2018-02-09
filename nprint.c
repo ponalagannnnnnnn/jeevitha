66f53c7499b21b9bb96c4ad60d8770272d1174ba
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-09 15:36:44 +0530

    Create nprint.c

---
+++ nprint.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int arr[10],i,n;
+printf("\n Enter the size of the array");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",%arr[i]);
+}
+printf("\n%d,%d",arr[i],[i]);
+getch();
+}
