36a91f6a69c6ecb75a790dbbe17fb921b4be28ad
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-09 15:50:40 +0530

    Create min.c

---
+++ min.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b,hr1 min1;
+printf("\n enter the time 1");
+scanf("%d\t%d",&hr,&min);
+printf("\n enter the time 2");
+scanf("%d\t%d",&hr1,min1);
+a=hr-hr1;
+b=min-min 1;
+printf("\nsub time%d%d",hr,min);
+}
+getch();
+}
+
+
