f8d760d666b86ac395633968587bd0c0315b0aa0
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-12 15:53:47 +0530

    Create maxo.c

---
+++ maxo.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int i,a[10],max;
+printf("\n enter the num");
+scanf("%d",&a);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=0,i<n;i++);
+{
+if(a[i]>max)
+{
+max=a[i]
+}
+}
+
+printf("\n max num is %d",max);
+getch();
+}
