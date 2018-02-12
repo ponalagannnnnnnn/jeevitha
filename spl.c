28dd27eb0efa63cd95767fe7b7d824ee8e5e4649
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-12 15:46:52 +0530

    Create spl.c

---
+++ spl.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{char str=size;
+char *s str;
+int alpha,digit,o,a;
+alpha=digit=o=a;
+printf("\n entr any string");
+gets(str);
+while(*s)
+{
+if(*s=>'a'&&*s<='z')
+alpha++;
+else(*s='0'&&*s<='9')
+digit++;
+else
+o++;
+s++;
+a=o;
+printf("\n %d",a);
+}
+getch();
+}
