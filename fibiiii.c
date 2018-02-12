f3336d638df0d05b01195d48a7ba51f463750dfd
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-12 16:01:32 +0530

    Create fibiiii.c

---
+++ fibiiii.c
@@ -0,0 +1,15 @@
+#incude<stdio.h>
+void main()
+{
+int a=0,b=1,i,c;
+printf("\n enter the numbers");
+scanf("%d%d",&a,&b);
+for(i=0;i<n;i++)
+{
+c=a+b;
+a=b;
+b=c;
+printf("\n %d",c);
+}
+getch();
+}
