7bd15b6c5f71fa98071ea70d2d18188a8843a96b
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-12 16:41:25 +0530

    Create wor.c

---
+++ wor.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("\n enter the number:");
+scanf("%d",&num);
+switch(n);
+{
+                 case1:printf("\n one");
+                 break;
+                 case2:printf("\n two");
+                 break;
+                 case3:printf(\n three");
+                 break;
+                 case4:printf("\n four");
+                 break;
+                 case5:printf("\n five");
+                 break;
+                 case6:printf("\n six");
+                 break;
+                 case7:printf("\n seven");
+                 break;
+                 case8:printf("\n eight");
+                 break;
+                 caes:printf("\n nine");
+                 break;
+                 default:printf("\n invalid input");
+                 break;
+                 }
+                 getch();
+                 }
+                
+         
+                 
+             
