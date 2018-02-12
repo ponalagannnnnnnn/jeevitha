a5bbff17081c261454b8f60f513f68010df849fb
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-12 16:09:14 +0530

    Create alnum.c

---
+++ alnum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char *s;
+printf("\n enter the string");
+if(*s=>'a'&&*s<='z')
+{
+printf("\n the gn str is alphabet");
+}
+else if(*s=>'0'&&*s<='9')
+{
+printf("\n the gn str is digit");
+}
+getch();
+}
