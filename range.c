7485f82b687ccc1db2514dde98368c61720b51cc
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-19 16:47:20 +0530

    Create range.c

---
+++ range.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int range(1-10),n;
+printf("\n enter the number");
+scanf("%d",num);
+if(n<=range(1-10))
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+}
+
