f2ea45217cf10521d99a6c0ffdf63c106e7fbd71
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-19 16:55:13 +0530

    Create nm.c

---
+++ nm.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               main?()
+{
+int n,m,p;
+printf("\n enter the value of n and m");
+scanf("%d%d",&n,&m");
+p=n+m;
+printf("\n the sum is %d",p);
+if(p%2==0)
+{
+printf("\n the result is even");
+}
+else
+{
+printf("\n the result is odd");
+}
+getch();
+}
