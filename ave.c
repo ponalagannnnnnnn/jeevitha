c11212096d11d95517df64b038d76f4760b7ffd7
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-19 16:07:13 +0530

    Create ave.c

---
+++ ave.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int num,c;
+printf("\n enter the value of n");
+scanf("%d",&num);
+while(num!=0)
+{
+c=n/2;
+}
+printf("\n the average value is %d",c);
+getch();
+}
