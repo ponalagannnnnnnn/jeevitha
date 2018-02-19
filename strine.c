acbcd43b8946e8fe2d70cd0b77e1e240f3e43858
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-19 16:30:13 +0530

    Create strine.c

---
+++ strine.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str1,str2;
+printf("\n Enter the string 1:");
+scanf("%s",&str1);
+printf("\n Enter the string 2:");
+scanf("%s",&str2);
+str=strcat(str1,str2);
+printf("\n the concatenated string is %s",str);
+getch();
+}
