4fa3c2051a9c47986ae84875d44c977e41c71982
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-19 16:42:12 +0530

    Create knum.c

---
+++ knum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char str;
+int k;
+printf("\n enter the string");
+scanf("%s",&str);
+printf("\n enter the value of k");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+printf("\n the strings are %s",i);
+}
+getch();
+}
