ba6531a8bbbd5ca09ef7ba16939dfc6607be777e
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-21 16:57:27 +0530

    Rename mul.c to mul0m.c

---
+++ mul0m.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int n,num,c,i,a;
+printf("\n ranges from1-9");
+scanf(%d",&n);
+printf("\n enter the number");
+scanf("%d",&num);
+if(a%10>=10)
+{
+printf("\n 10")
+}
+for(i=1,i<n;i++)
+{
+c=10*i;
+}
+printf("%d",  c);
+}
+getch();
+}
