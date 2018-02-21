f383ed1e9ba649e7738fa92a82f2a6537eb17f65
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-21 15:39:29 +0530

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int a;
+char str;
+printf("\n enter the string");
+scanf("%s",&str);
+a=1 and 0;
+if(str==a)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+}
