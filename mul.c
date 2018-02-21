14194897c62dbb388c430addfb81d9e16333b8fc
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-21 16:15:53 +0530

    Create mul.c

---
+++ mul.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the value of a");
+scanf("%d",&a);
+if(a%7==0)
+{
+printf("\n it is a multiple of 7");
+}
+else
+{
+printf("\n it is not a multiple of 7");
+}
+getch();
+}
