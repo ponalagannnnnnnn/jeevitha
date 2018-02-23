1f9729feb34caac4dfe239d79d64c1e17fc79728
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-23 15:42:46 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+char a,lower case,upper case,c; 
+printf("\n enter the string");
+scanf("%s",a);
+lower case={'a','e','i','o','u'};
+upper case={'A','E','I','O','U'};
+c=lower case||upper case;
+if(a==c)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n No");
+}
+getch();
+}
