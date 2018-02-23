445f6fa679122b5e3a7f817bfb4e0b1d53aad51a
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-02-23 16:44:23 +0530

    Create PERFECT.C

---
+++ PERFECT.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,b
+printf("\n enter the number of a and b");
+scanf("%d%d",&a,&b);
+c=a*b;
+if(c==perfect sqr)
+{
+printf("\n it is a perfect sqr");
+}
+else
+{
+printf("\n not a perfect sqr");
+}
+getch();
+}
