99453b10d8e1683d7ecb54a28fd1f8288e2c6ad5
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-03-05 15:54:17 +0530

    Create composite.c

---
+++ composite.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int nu,i,flag;
+printf("\n enter the number");
+scanf("%d",&num);
+for(i=2;i<=num/2;i++)
+{
+if(num%i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+printf("\n no");
+}
+else
+{
+printf("\n yes");
+}
+return 0;
+}
