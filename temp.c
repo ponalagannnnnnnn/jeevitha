57671b456c892eadcdeed2ee906c5d0613735df2
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-03-05 16:45:38 +0530

    Create temp.c

---
+++ temp.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+float celcius;
+float kelvin;
+printf("\n enter the value of temperature in celsius");
+scanf(%f",&celcius);
+kelvin=celcius+293;
+printf("\n the value in kelvin is %f",kelvin);
+getch();
+}
