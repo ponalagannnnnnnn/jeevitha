4fd1efdb0024d239f6bde703f9be6b0716902f1f
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-03-12 16:15:01 +0530

    Create nth.c

---
+++ nth.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i,n,k,a[10],i;
+printf("\n enter the number n, k");
+scanf("%d\t%d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("%d",a[k-1]);
+getch();
+}
