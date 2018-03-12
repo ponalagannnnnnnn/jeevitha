84c9e65f9ed7ffb09f5d1f689508b210c60b5cac
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-03-12 15:54:48 +0530

    Create CAPS.C

---
+++ CAPS.C
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#define MAX 100
+void main()
+{
+char str[MAX]={0};	
+	int i;
+	printf("Enter a string: ");
+	scanf("%[^\n]s",str); 
+	for(i=0; str[i]!='\0'; i++)
+	{
+		
+		if(i==0)
+		{
+			if((str[i]>='a' && str[i]<='z'))
+				str[i]=str[i]-32; 
+        continue; 
+		}
+		if(str[i]==' ')
+		{
+				++i;
+			if(str[i]>='a' && str[i]<='z')
+			{
+				str[i]=str[i]-32; 
+				continue; 
+        }
+		}
+		else
+		{
+			if(str[i]>='A' && str[i]<='Z')
+				str[i]=str[i]+32; 
+	}
+	printf("Capitalize string is: %s\n",str);
+}
+getch();
+}
