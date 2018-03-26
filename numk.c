9dc747e7e6ac88d5367a247be79d9143f8fbdb71
ponalagannnnnnnn <35526209+ponalagannnnnnnn@users.noreply.github.com>
2018-03-26 18:52:21 +0530

    Create numk.c

---
+++ numk.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+int n,a[10],i,k,temp,j;
+printf("\n enter the no.of values and k value:");
+scanf("%d%d",&n,&k);
+printf("\n enter the values:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+  for(i=0;i<n;i++)
+  {
+    for(j=i+1;j<n;j++)
+    {
+         if(a[i]>a[j+1])
+        {  
+           temp=a[i];
+          a[i]=a[j+1];
+          a[j]=temp;
+         }
+    }
+  }
+  printf("\n %d",a[k]);
+  getch();
+}
+ 
